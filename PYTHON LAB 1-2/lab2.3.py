221884e6ecd5b72289ac268e704d45f33f43022e
Dhruvnariya25 <dhruvnariya25@gmail.com>
2025-02-04 21:09:39 +0530

    Add files via upload

---
+++ PYTHON LAB 1-2/lab2.3.py
@@ -0,0 +1,39 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=================================================== RESTART: C:/Users/Dhruv/AppData/Local/Programs/Python/Python313/PYTHON/lab2.3.py ===================================================
+ENTER ANY NUMBER :5
+number is odd
+>>> 
+=================================================== RESTART: C:/Users/Dhruv/AppData/Local/Programs/Python/Python313/PYTHON/lab2.3.py ===================================================
+ENTER ANY NUMBER :6
+number is even
+ENTER ANY NUMBER :10
+number is even
+ENTER ANY NUMBER :7
+number is odd
+>>> 
+>>> 
+=================================================== RESTART: C:/Users/Dhruv/AppData/Local/Programs/Python/Python313/PYTHON/lab2.3.py ===================================================
+ENTER ANY NUMBER :
+=================================================== RESTART: C:/Users/Dhruv/AppData/Local/Programs/Python/Python313/PYTHON/lab2.4.py ===================================================
+ENTER ANY NUMBER :10
+number is divisable by 10
+>>> 
+=================================================== RESTART: C:/Users/Dhruv/AppData/Local/Programs/Python/Python313/PYTHON/lab2.4.py ===================================================
+ENTER ANY NUMBER :
+Traceback (most recent call last):
+  File "C:/Users/Dhruv/AppData/Local/Programs/Python/Python313/PYTHON/lab2.4.py", line 9, in <module>
+    b4()
+  File "C:/Users/Dhruv/AppData/Local/Programs/Python/Python313/PYTHON/lab2.4.py", line 2, in b4
+    a=int(input("ENTER ANY NUMBER :"))
+ValueError: invalid literal for int() with base 10: ''
+>>> 
+>>> 
+=================================================== RESTART: C:/Users/Dhruv/AppData/Local/Programs/Python/Python313/PYTHON/lab2.4.py ===================================================
+ENTER ANY NUMBER :5
+number is not divisable by 10
+>>> 
+=================================================== RESTART: C:/Users/Dhruv/AppData/Local/Programs/Python/Python313/PYTHON/lab2.4.py ===================================================
+ENTER ANY NUMBER :50
+number is divisable by 10
